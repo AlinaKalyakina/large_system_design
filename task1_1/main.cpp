aba991764a849bc5a052bb0ef178cce3a97e06bd
Alina <alinka.kalyakina@yandex.ru>
2020-09-20 19:00:12 +0300

    Initial commit

---
+++ task1_1/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <array>
+
+template <int N>
+constexpr int det(const std::array<std::array<int, N>, N>& a) {
+    int m = 1;
+    int res = 0;
+    for (int i = 0; i < N; i++) {
+        std::array<std::array<int, N-1>, N-1> new_array {};
+        for (int k = 0; k < N-1; k++) {
+            int real_l = 0;
+            for (int l = 0; l < N; l++) {
+                if (l != i) {
+                    (&std::get<0>((&std::get<0>(new_array))[real_l]))[k] = a[k+1][l];// new_array[k][real_l] = a[k+1][l];
+                    real_l++;
+                }
+            }
+        }
+        res += m*a[0][i] * det<N-1>(new_array);
+        m *= -1;
+    }
+    return res;
+}
+
+template <>
+constexpr int det<1>(const std::array<std::array<int, 1>, 1>& a) {return a[0][0];}
+
+
+int main() {
+    constexpr std::array<std::array<int, 3>, 3> A = {{
+                                                   {0, 1, 2}
+                                                   ,{1, 2, 3}
+                                                   ,{2, 3, 7}
+                                           }};
+    constexpr int res = det<3>(A);
+    static_assert(res == -3);
+}
