691b367dc2c2cbe7964b9d45f2c3ad83672977f6
Alina <alinka.kalyakina@yandex.ru>
2020-10-07 01:37:31 +0300

    vector+set and tuple+pair united

--- task2/main.cpp
+++ task2/main.cpp
@@ -1,10 +1,10 @@
+//Только >= с++17!
 #include <iostream>
 #include <vector>
 #include <string>
 #include <tuple>
 #include <sstream>
 #include <set>
-#include <map>
 
 struct Printer {
     std::string str() const {
@@ -19,58 +19,55 @@ struct Printer {
 
     template <typename T1, typename T2>
     Printer& format(const std::pair<T1, T2>& v) {
-        ss << '(';
-        format(v.first);
-        format(',');
-        format(v.second);
-        ss << ')';
+        ss << "(";
+        tuple_format<0>(std::make_tuple(v.first, v.second));
+        ss << ")";
         return *this;
     }
 
-    template <typename T>
-    Printer& format(const std::vector<T>& v) {
-        ss << "[ ";
+    template<template<typename> class ContainerT, typename ValueT>
+    Printer& format(const ContainerT<ValueT>& v) {
+        if constexpr (std::is_same<ContainerT<ValueT>, std::string>::value) {
+            ss << v;
+            return *this;
+        }
+        if constexpr (std::is_same<ContainerT<ValueT>, std::vector<ValueT>>::value ) {
+            ss << "[ ";
+        } else {
+            ss << "( ";
+        }
         for (auto& x : v ) {
             format(x);
-            format(", ");
+            ss << ", ";
         }
         ss.seekp(-2, std::stringstream::cur);
-        ss << " ]";
-        return *this;
-    }
-    template <typename T>
-    Printer& format(const std::set<T>& v) {
-        ss << "{ ";
-        for (auto& x : v ) {
-            format(x);
-            format(", ");
+        if constexpr (std::is_same<ContainerT<ValueT>, std::vector<ValueT>>::value ) {
+            ss << " ]";
+        } else {
+            ss << " )";
         }
-        ss.seekp(-1, std::stringstream::cur);
-        ss << "} ";
         return *this;
     }
 
     template <typename...Args>
     Printer& format(const std::tuple<Args...>& v) {
         ss << "( ";
-        tuple_format<0, Args...>(v);
-        ss.seekp(-2, std::stringstream::cur);
+        tuple_format<0>(v);
         ss << " )";
         return *this;
     }
 
-    template<int Pos, typename...Args>
-    void tuple_format(const std::tuple<Args...>& v) {
-        if constexpr (Pos > sizeof...(Args) - 1) {
+    template<int Pos, template<typename> class ContainerT, typename...Args>
+    void tuple_format(const ContainerT<Args...>& v) {
+        if constexpr (Pos >= sizeof...(Args)) {
+            ss.seekp(-2, std::stringstream::cur);
             return;
         } else {
             format(std::get<Pos>(v));
-            format(", ");
-            tuple_format<Pos + 1>(v);
+            ss << ", ";
+            tuple_format<Pos + 1, ContainerT, Args...>(v);
         }
     }
-
-
 private:
     std::stringstream ss;
 };
@@ -84,6 +81,7 @@ int main() {
     std::tuple<std::string, int, int> t = {"xyz", 1, 2};
     std::vector<std::pair<int, int> > v = {{1, 4}, {5, 6}};
     std::string s1 = Printer().format(" vector: ").format(v).str();
+    std::pair<int, int> m = {1, 1};
 // " vector: [ (1, 4), (5, 6) ]"
     std::string s2 = Printer().format(t).format(" ! ").format(0).str();
     std::cout << s1 << std::endl << s2;
